800eb4bee8f60b33923f5678ada575ba826efb65
pamanac <patrickmanacorda@gmail.com>
2021-08-13 16:30:53 -0700

    Templated dotnet app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Compbuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
